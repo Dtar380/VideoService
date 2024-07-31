b260f5a5c53d326892299e990297c6a7dbcab65e
David Tarin <dtar380@icloud.com>
2024-07-31 22:21:50 +0200

    Imagine

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def main():
         author="Dtar380",
         license="MIT",
         classifiers=[
-            "License :: MIT License",
+            "License :: OSI Approved :: MIT License",
             "Programming Language :: Python :: 3.12",
             "Operating System :: OS Independent"
         ],
